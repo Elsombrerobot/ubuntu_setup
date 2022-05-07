bbf9b5f5d945da34e6937bbe5f1c9263d64dc17b
Marc Ferrer <iammarcferrer@gmail.com>
2022-05-07 11:02:01 +0200

    Changes

---
+++ ubuntu_setup.sh
@@ -0,0 +1,33 @@
+# Update all
+sudo apt update && sudo apt full-upgrade -y
+
+# Softwares install
+sudo apt-get -y install wget
+sudo apt-get -y install git
+sudo apt-get -y install vim
+sudo apt-get -y install zsh
+sudo snap install discord
+sudo snap install spotify
+sudo snap install bitwarden
+sudo snap install signal-desktop
+sudo snap install code
+sudo snap install vlc
+
+# More complex install
+# Vim plug 
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+#Install nerd font
+wget -P ~/Downloads https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/CascadiaCode.zip
+unzip DroidSansMono.zip -d ~/.local/share/fonts
+fc-cache -fv
+
+#Oh my posh 
+sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+sudo chmod +x /usr/local/bin/oh-my-posh
+
+
+#make zsh default
+chsh -s $(which zsh)
+
+
